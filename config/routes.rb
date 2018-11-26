4eb14b341b3a20c1d8361912320179bd147ebd51
mizssy07 <mizssy07@gmail.com>
2018-11-26 21:49:42 +0900

    before bootstrap apply

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "home#top"
+
+  get "topics/index" => "topics#index"
+  get "topics/show/:id" => "topics#show"
+  post "topics/create" => "topics#create"
+  post "topics/delete/:id" => "topics#delete"
+
+  post "posts/create" => "posts#create"
+  post "posts/delete/:id" => "posts#delete"
+
 end
