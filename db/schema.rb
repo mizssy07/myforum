6055aa14c9472676f6be741289194014854cc3d9
mizssy07 <mizssy07@gmail.com>
2018-11-29 09:20:02 +0900

    created user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_015134) do
+ActiveRecord::Schema.define(version: 2018_11_29_001803) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_11_23_015134) do
 
   create_table "topics", force: :cascade do |t|
     t.text "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
