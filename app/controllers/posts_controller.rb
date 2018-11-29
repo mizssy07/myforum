47fcc0c6b33284ec8f80e6b1524d1aa3b373e759
mizssy07 <mizssy07@gmail.com>
2018-11-29 09:06:40 +0900

    merged after_bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order(created_at: :asc)
+  end
+
+  def create
+    @post = Post.new(name: params[:name], content: params[:content], topic_id: params[:topic_id].to_i)
+    if @post.save
+      redirect_to("/topics/show/#{@post.topic_id}")
+    else
+      render("topics/show/#{params[:topic_id].to_i}")
+    end
+  end
+
+  def delete
+    @post = Post.find_by(id: params[:id])
+    @post.name = "削除されました"
+    @post.content = "削除されました"
+    @post.save
+    redirect_to("/topics/show/#{@post.topic_id}")
+  end
+  
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,28 @@
 class TopicsController < ApplicationController
+
+  def index
+    @topics = Topic.all.order(updated_at: :desc)
+  end
+
+  def create
+    @topic = Topic.new(title: params[:title])
+    @topic.save
+    redirect_to("/topics/index")
+  end
+
+  def show
+    @topic = Topic.find_by(id: params[:id])
+    @posts = Post.where(topic_id: params[:id]).order(created_at: :asc)
+  end
+
+  def delete
+    @topic = Topic.find_by(id: params[:id])
+    @posts = Post.where(topic_id: @topic.id)
+    @posts.each do |post|
+      post.destroy
+    end
+    @topic.destroy
+    redirect_to("/topics/index")
+  end
+
 end
