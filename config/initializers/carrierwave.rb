11000daeefa4cff667fd4f720b87f4f0d02bac4b
mizssy07 <mizssy07@gmail.com>
2018-12-10 21:42:00 +0900

    post image files

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+
+  config.asset_host = "http://localhost:3000" # or Settings.url
+
+  if Rails.env.development? or Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => '<access key>',
+      :aws_secret_access_key  => '<secrect acess key >'
+    }
+    config.fog_directory  = "<name of your bucket>"
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+  end
+end
