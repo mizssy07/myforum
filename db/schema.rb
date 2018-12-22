94e163860bdee5b35e80a6817c05eb037d376674
shunya mizuno <mizssy07@gmail.com>
2018-12-22 15:19:24 +0900

    fix columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_130756) do
+ActiveRecord::Schema.define(version: 2018_12_22_061751) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
