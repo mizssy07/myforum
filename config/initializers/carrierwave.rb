9b3fe52ed45a1bf0488eff72bdac5760af306c65
shunya mizuno <mizssy07@gmail.com>
2018-12-25 23:39:08 +0900

    fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,13 +10,8 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV['SECRET_ACCESS_KEY'],
       :region                 => 'ap-northeast-1'
     }
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
-    config.storage = :fog
-    config.fog_public = true
     config.fog_directory  = ENV['S3_BUCKET_NAME']
-    config.cache_dir =  :fog
-    config.cache_storage = :fog
-    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/rails-picture-181225'
+    config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
   end
 
 end
