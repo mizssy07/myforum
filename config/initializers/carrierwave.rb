fa53be31d21cf944b6db63629bb56e83401ddfb5
shunya mizuno <mizssy07@gmail.com>
2018-12-25 23:22:14 +0900

    fix carrierwave.rb and image_uploader and image_name.to_s

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,17 +4,18 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.asset_host = "http://localhost:3000"
   else
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['SECRET_ACCESS_KEY'],
       :region                 => 'ap-northeast-1'
     }
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.storage = :fog
     config.fog_public = true
-    config.fog_directory  = 'rails-picture-181225'
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.cache_dir =  :fog
+    config.cache_storage = :fog
     config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/rails-picture-181225'
   end
 
